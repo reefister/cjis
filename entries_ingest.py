5b635195beaff2f41faf77d0d01751aeb8e671bc
Ashwin <ashwinbala2107@gmail.com>
2026-05-10 12:48:28 +0530

    feat: initial journal ingestion pipeline with PostgreSQL

---
+++ entries_ingest.py
@@ -0,0 +1,58 @@
+import json 
+import psycopg2
+from datetime import datetime
+
+with open('Reverie.json','r', encoding='utf-8') as file:
+    data = json.load(file)
+
+
+entries = data['entries']
+
+
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="cjis",
+    user="postgres",
+    password="CJIS",
+    port="5432"
+)
+cursor = conn.cursor()
+
+insert_query = """
+INSERT INTO journal_entries (dayone_id, date, raw_text, raw_json)
+VALUES (%s, %s, %s, %s);
+"""
+
+
+for entry in entries:
+    text = entry.get('text')
+    uuid = entry.get('uuid')
+    creation_date_str = entry.get('creationDate')
+    raw_json = entry
+
+    if not text or not uuid or not creation_date_str:
+        continue
+
+    creation_date = datetime.fromisoformat(creation_date_str.replace("Z", "+00:00"))
+
+    print("UUID:", uuid)
+    print("Created:", creation_date)
+    print("Text preview:", text[:80])
+    print("Raw JSON keys:", list(raw_json.keys()))
+
+    cursor.execute(
+    insert_query,
+    (
+        uuid,
+        creation_date,
+        text,
+        json.dumps(raw_json)
+    )
+    )
+
+    conn.commit()
+    break
+
+cursor.close()
+conn.close()
