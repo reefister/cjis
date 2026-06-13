b694105007436a97018fa710abd6bd4922792bfe
Ashwin <ashwinbala2107@gmail.com>
2026-06-13 23:55:30 +0530

    concepts column ingestion

---
+++ ingestion/concepts_ingest.py
@@ -0,0 +1,57 @@
+import os
+import json
+from groq import Groq
+from dotenv import load_dotenv
+from app.db import get_connection
+load_dotenv()
+
+client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
+
+def extract_concepts(text):
+    """Sends the journal text to Groq and returns a comma-separated string of concepts."""
+    if not text:
+        return ""
+    try:
+        completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content": (
+                        "you're reviewing a journal to detect concepts per entry in the journal, this data will be used for deeper analysis, so your task is to accurately detect concepts for each entry, return only a JSON array of strings, nothing else."
+                       
+                    )
+                },
+                {"role": "user", "content": str(text)}
+            ],
+            model="llama-3.3-70b-versatile",
+            temperature=0.3,
+        )
+        return completion.choices[0].message.content.strip()
+    except Exception as e:
+        print(f"Groq API Error: {e}")
+        return None
+
+
+conn = get_connection()
+cursor = conn.cursor()
+
+get_query = """select * from journal_entries"""
+
+cursor.execute(get_query)
+
+tuple_rows = cursor.fetchall()
+
+update_query = """UPDATE entry_features SET concepts = %s WHERE entry_id = %s AND concepts IS NULL"""
+
+for id_value,dayone_id,date,raw_text_value,raw_json,created_at in tuple_rows:
+
+    entry_id = id_value
+    concepts_json_array = extract_concepts(raw_text_value)
+    concepts = json.loads(concepts_json_array)
+    cursor.execute(update_query,(json.dumps(concepts),entry_id))
+
+    
+conn.commit()
+cursor.close()
+conn.close()
+
