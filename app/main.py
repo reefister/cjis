2485ce734e0d89a6e599c62e04b44dac092510d6
Ashwin <ashwinbala2107@gmail.com>
2026-07-05 23:33:48 +0530

    Concept Cooccurence endpoint configured

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.routes.entries import entries_router
 from app.routes.search import search_router
-from app.routes.analytics import analytics_sentiment_router, analytics_concept_frequency_router
+from app.routes.analytics import analytics_sentiment_router, analytics_concept_frequency_router, analytics_concept_coocurrence_router
 
 
 
@@ -11,6 +11,7 @@ app.include_router(entries_router)
 app.include_router(search_router)
 app.include_router(analytics_sentiment_router)
 app.include_router(analytics_concept_frequency_router)
+app.include_router(analytics_concept_coocurrence_router)
 
 @app.get("/ping")
 def test_check():
--- app/routes/analytics.py
+++ app/routes/analytics.py
@@ -3,6 +3,7 @@ from app.db import get_connection
 
 analytics_sentiment_router = APIRouter()
 analytics_concept_frequency_router = APIRouter()
+analytics_concept_coocurrence_router = APIRouter()
 
 @analytics_sentiment_router.get("/analytics/sentiment")
 def sentiment_analytics(word):
@@ -49,5 +50,45 @@ def concept_frequency():
     cursor.close()
     conn.close()
     return retrieved_list
+
+@analytics_concept_coocurrence_router.get("/analytics/concept/cooccurence")
+def concept_coocurrence(word):
+    conn = get_connection()
+    cursor = conn.cursor()
+    get_query = """with matched_entries as (
+SELECT 
+	e.*,j.*
+FROM 
+    entry_features e join journal_entries j on e.entry_id = j.id,
+	jsonb_array_elements_text(e.concepts) AS concept
+WHERE 
+	concept ilike %s
+)
+
+select lower(concept),count(*) as co_occurence_count
+from matched_entries m,jsonb_array_elements_text(m.concepts) AS concept
+
+where lower(concept) != %s
+group by lower(concept)
+order by co_occurence_count desc"""
+    params = []
+    
+    params.append(word)
+    params.append(word.lower())
+
+    cursor.execute(get_query,tuple(params))
+    tuple_rows = cursor.fetchall()
+    
+    retrieved_list = []
+    for concept,count in tuple_rows:
+        dict_row = {}
+        dict_row['concept'] = concept
+        dict_row['count'] = count
+        retrieved_list.append(dict_row)
+    
+    cursor.close()
+    conn.close()
+    return retrieved_list
+
 
 
